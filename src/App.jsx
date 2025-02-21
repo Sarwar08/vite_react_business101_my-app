9a558dcadd4ba355546849da44329b73979d4f14
Sarwar Habib <engr.sarwarhabib@gmail.com>
2025-02-21 15:29:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+
+      <div className="border border-yellow-600 flex justify-center gap-4 p-4">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <br />
+        <button className="btn btn-primary"> Button </button>
+      </div>
+      </div>
+    </>
+
+  )
+}
+
+export default App
